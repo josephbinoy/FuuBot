e92802ccd07073836e5d508d14c3fd8247898fdc
JBM <josephbinoy110@gmail.com>
2024-02-08 16:18:51 +0530

    deleted dist

--- dist/trials/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-//import * as trial from './WebServerTrial.js';
-//trial.webServerTrial();
-Object.defineProperty(exports, "__esModule", { value: true });
-const DiscordTrial_1 = require("./DiscordTrial");
-(0, DiscordTrial_1.trial)();
-//# sourceMappingURL=index.js.map
